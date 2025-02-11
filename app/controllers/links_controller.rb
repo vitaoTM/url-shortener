361e23f72fa744fee53afd2b4d069eb890310e1c
vitor <vitor.grosskopf@gmail.com>
2025-02-11 16:55:03 -0300

    Add #create action to LinksController

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,21 @@
 class LinksController < ApplicationController
   def index
-    @links = Link.all
+    @links = Link.recent_first
+  end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to root_path
+    else
+      index
+      render :index, status: :unprecessable_entity
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:url)
   end
 end
