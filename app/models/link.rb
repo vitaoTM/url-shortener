ceb0cb191a716c7d6455fca6c918c60537249f2f
vitor <vitor.grosskopf@gmail.com>
2025-02-17 16:14:22 -0300

    Link model: Overwrite find and to_param for Base62

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,12 @@
 class Link < ApplicationRecord
   scope :recent_first, -> { order(created_at: :desc) }
   validates :url, presence: true
+
+  def self.find(id)
+    super Base62.decode(id)
+  end
+
+  def to_param
+    Base62.encode(id)
+  end
 end
