4b23345538f65fae726e89a2bb76849f609a9292
vitor <vitor.grosskopf@gmail.com>
2025-03-03 16:55:56 -0300

    Add spec test to Metadata model

---
+++ spec/models/metadata_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe Metadata, type: :model do
+  context "Title attribute" do
+    it 'Has a title attribute' do
+      metadata = Metadata.new("<title>Hello World</title>").title
+      expect(metadata).to eq('Hello World')
+    end
+    it 'Missing title attribute' do
+      metadata = Metadata.new.title
+      expect(metadata).to be_nil
+    end
+  end
+  context 'Meta description' do
+    it 'Has description' do
+      metadata = Metadata.new("<meta name='description' content='Hello'").description
+      expect(metadata).to eq('Hello')
+    end
+    it 'Missing description' do
+      metadata = Metadata.new.description
+      expect(metadata).to be_nil
+    end
+  end
+  context 'Image description' do
+    it 'image' do
+      metadata = Metadata.new("<meta name='og:image' content='https://example.org/favicon.ico'").image
+      expect(metadata).to eq('https://example.org/favicon.ico')
+    end
+    it 'Missing image' do
+      metadata = Metadata.new.image
+      expect(metadata).to be_nil
+    end
+  end
+end
