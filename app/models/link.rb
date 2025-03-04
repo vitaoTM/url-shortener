2cc98eebd7afb0d943feb8a8b54370b39a9f8d9c
vitor <vitor.grosskopf@gmail.com>
2025-03-04 16:04:29 -0300

    Add after_save_commit hook to Links model
    - Perform background job later for metadata (?)

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,11 +3,19 @@ class Link < ApplicationRecord
   scope :recent_first, -> { order(created_at: :desc) }
   validates :url, presence: true
 
+  after_save_commit if: :url_previously_changed? do
+    MetadataJob.perform_later(to_param)
+  end
+
   def self.find(id)
     super Base62.decode(id)
   end
 
   def to_param
     Base62.encode(id)
+  end
+
+  def domain
+    URI(url).host rescue URI::InvalidURIError
   end
 end
