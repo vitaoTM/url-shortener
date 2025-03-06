d8bb8a54577f9e9036e427ea0bbc81951a43a801
vitor <vitor.grosskopf@gmail.com>
2025-03-06 16:18:20 -0300

    Add copy to clipboard buttons

---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = {
+    content: String
+  }
+
+  connect() {
+    this.originalText = this.element.textContent
+  }
+
+  copy() {
+    navigator.clipboard.writeText(this.contentValue).then(
+      () => {
+        this.element.textContent = "Copied!"
+        setTimeout(() => {
+          this.element.textContent = this.originalText
+        }, 1000
+      )
+      },
+      () => {
+        alert("Fail to copy to clipboard")
+      }
+    )
+        // (clipText) => (document.querySelector(".clip-text").innerText = clipText),
+  // );
+  }
+}
