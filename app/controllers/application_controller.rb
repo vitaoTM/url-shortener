d0e68d163cc72fbd00f497485defc014c162d1e7
vitor <vitor.grosskopf@gmail.com>
2025-04-17 17:10:43 -0300

    Updating application controller, setting before_action devise authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
+  before_action :authenticate_user!
   def set_link
     @link = Link.find(params[:id])
   end
